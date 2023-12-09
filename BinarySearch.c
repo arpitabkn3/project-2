7e9bee3252d972dbb898d1092bd9c543fdce7261
arpitabkn3 <151006850+arpitabkn3@users.noreply.github.com>
2023-12-10 00:16:34 +0530

    Create BinarySearch.c

---
+++ BinarySearch.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+int bs(int arr[],int x,int low,int high)
+{
+    while(low<=high)
+    {
+        int mid(low +h igh)/2;
+        if (arr[mid]==x);
+        {
+            return mid;
+             if(arr[mid]<=x);
+             low=mid+1;
+
+        else
+
+             high=mid-1;}
+             return-1;
+    }
+    int main()
+    {
+        int array[6]={10,1,23,224,455};
+        int x=size of (arr)/size of (arr[0]);
+        int x=4;
+        int r=bs(arr,x,0,n-1);
+        if(result=-1)
+        printf("not found");
+        printf("elements found at index %d",r);
+        getch();
+
+    }
+}
